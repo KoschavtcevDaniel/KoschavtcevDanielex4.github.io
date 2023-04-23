0c301de049e21b7b0eef5c35ede2972923b561e6
KoschavtcevDaniel <112947715+KoschavtcevDaniel@users.noreply.github.com>
2023-04-23 21:11:17 +0300

    Update form.php

--- form.php
+++ form.php
@@ -34,11 +34,16 @@ if (!empty($messages)) {
   <input name="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print strip_tags($values['email']); ?>" /></div>
   <div class = "year"><label>YEAR</label>
   <select name="year" <?php if ($errors['year']) {print 'class="error"';} ?> value="<?php print strip_tags($values['year']); ?>">
-    <?php 
-    for ($i = 1922; $i <= 2022; $i++) {
-      printf('<option value="%d">%d год</option>', $i, $i);
+  <?php
+       for($i=1920;$i<=2023;$i++){
+         if($values['year']==$i){
+          printf("<option value=%d selected>%d </option>",$i,$i);
+            }
+        else{
+          printf("<option value=%d>%d </option>",$i,$i);
+        }
     }
-    ?>
+   ?>
   </select></div>
   <div class = "sex" <?php if ($errors['sex']) {print 'class="error"';} ?>><label>Your SEX?</label>
   <label>Man</label><input type="radio" checked="checked" name="sex" value="0" <?php if($values['sex']=="male") {print 'checked';} ?>/>
@@ -55,7 +60,7 @@ if (!empty($messages)) {
   </div>
 
   <div class = "ability">  
-  <select name="abilities[]" multiple="multiple" <?php if ($errors['ability']) {print 'class="error"';} ?>> 
+  <select name="abilities[]" multiple="multiple" <?php if ($errors['abilities']) {print 'class="error"';} ?>> 
     <option value="ab_in" <?php if($values['ab_in']==1){print 'selected';} ?>>невидимость</option>
     <option value="ab_t" <?php if($values['ab_t']==1){print 'selected';} ?>>телепортация</option>
     <option value="ab_l" <?php if($values['ab_l']==1){print 'selected';} ?>>левитация</option>
